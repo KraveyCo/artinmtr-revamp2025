59cb2bf0088a2bd3f4404a0998fe13d9cf920575
Your Name <your.email@example.com>
2025-04-07 15:08:03 +0800

    Fix GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,10 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
